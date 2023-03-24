2ec04a39f6710067ef075099c7f28ce9925a9924
Jason Starkman <jrstarkman@gmail.com>
2023-03-23 22:58:55 -0400

    Exercise 2.5

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,33 +1,4 @@
-const Header = ({course}) => <h2>{course}</h2>
-
-const Total = ({parts}) => {
-  var sum = parts.reduce((total, curPart) => {
-    return total + curPart.exercises
-  },0)
-
-  return (
-    <b>Total of {sum} exercises</b>
-  )
-}
-
-const Part = ({part}) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Course = ({course}) => {
-  console.log(course.name);
-  return(
-    <div>
-    <Header course={course.name} />
-    {course.parts.map( part =>
-      <Part key={part.id} part={part}></Part>
-    )}
-    <Total parts={course.parts} />
-    </div>
-  )
-
-}
+import Course from "./Course"
 
 const Courses = ({courses}) => {
   return courses.map(course => (
