412c08b445179b58ee54e11791c0b2d190682ef7
Jason Starkman <jrstarkman@gmail.com>
2023-04-02 19:18:31 -0400

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Filter = (props) =>{
   return (
@@ -48,18 +50,23 @@ const PeopleView = ({filter, persons}) =>{
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
 
   const [newNumber, setNewNumber] = useState('')
 
   const [filter, setFilter] = useState('')
 
+  useEffect(() =>{
+    console.log('effect');
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response =>{
+      setPersons(response.data)
+    })
+  }, [])
+  
+  console.log('render', persons.length, 'notes')
 
   const handleNewPerson = (event) =>{
     event.preventDefault()
