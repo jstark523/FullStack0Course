0380f9111d7b6fe0e53983f130a91f51b1695e04
Jason Starkman <jrstarkman@gmail.com>
2023-04-24 00:27:14 -0400

    Exercise 2.20

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,23 @@
 import { useEffect, useState} from 'react'
 import axios from 'axios'
 
-const Result = ({result, setResult}) =>{
-  if(result !== null){
+const Weather = ({weather, got}) =>{
+  if (weather !== null && got === true){
+    return (
+      <>
+      <p>temperature {weather.main.temp} Celsius</p>
+      <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+      <p>wind {weather.wind.speed} m/s</p>
+      </>
+    );
+  }
+}
+
+const Result = ({result, setResult, setGot}) =>{  
+  if(result.length !== 0){
     if(result.length === 1){
       const languages = Object.values(result[0].languages);
+
       return (
         <div>
           <h1>{result[0].name.common}</h1>
@@ -21,10 +34,12 @@ const Result = ({result, setResult}) =>{
           {result[0].flag}
           </font>
           </p>
+          <h2>Weather in {result[0].name.common}</h2>
         </div>
       );
     }
     else if(result.length <= 10 && result.length > 1){
+      setGot(false);
       return (
         <div>
           {result.map((country) => 
@@ -34,6 +49,7 @@ const Result = ({result, setResult}) =>{
       );
     }
     else{
+      setGot(false);
       return (
         <p>Too many matches, please be more specific</p>
       );
@@ -42,9 +58,14 @@ const Result = ({result, setResult}) =>{
 }
 
 function App() {
+  const api_key = process.env.REACT_APP_API_KEY
+
   const [countryData, setCountryData] = useState(null);
   const [search, setSearch] = useState('');
-  const [result, setResult] = useState(null);
+  const [result, setResult] = useState([]);
+  const [weather, setWeather] = useState(null);
+  const [got, setGot] = useState(false);
+
 
   useEffect(() =>{
     axios.get('https://restcountries.com/v3.1/all')
@@ -56,6 +77,16 @@ function App() {
     });
   },[])
 
+  useEffect(() => {
+    if (result.length === 1){
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=${api_key}&lat=${result[0].latlng[0]}&lon=${result[0].latlng[1]}&units=metric`)
+      .then(response =>{
+        setWeather(response.data);
+        setGot(true);
+    })}
+  }, [result])
+
+
   const searchFunc = () =>{
     const filteredCountries = countryData.filter(country => country.name.common.toLowerCase().includes(search));
     setResult(filteredCountries);
@@ -65,13 +96,16 @@ function App() {
     setSearch(event.target.value.toLowerCase());
     searchFunc();
   }
+  
+
 
   return (
     <div className="App">
       <p>
       find countries<input onChange={handleSearchChange}></input>
       </p>
-      <Result result = {result} setResult = {setResult}/>
+      <Result result = {result} setResult = {setResult} setGot = {setGot}/>
+      <Weather weather={weather} got={got}/>
     </div>
   );
 }
