6cdbac370f01bec6b53f9c3f28e42c425bd7d1bf
Jason Starkman <jrstarkman@gmail.com>
2023-06-06 14:59:02 -0400

    Exercise 3.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -116,7 +116,7 @@ const App = () => {
   useEffect(() =>{
     console.log('effect');
     axios
-    .get('http://localhost:3001/persons')
+    .get('http://localhost:3001/api/persons')
     .then(response =>{
       setPersons(response.data)
     })
